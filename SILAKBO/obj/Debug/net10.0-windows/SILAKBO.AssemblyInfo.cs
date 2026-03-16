386e2abc5ad4c5752d199505860f2bde7a608849
smal610 <laurelsarina@gmail.com>
2026-03-17 01:39:03 +0800

    Register button fixed.

--- SILAKBO/obj/Debug/net10.0-windows/SILAKBO.AssemblyInfo.cs
+++ SILAKBO/obj/Debug/net10.0-windows/SILAKBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237e303c3365a74e8e8e9e1538df0d88b8aa08af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d8b6989cf2480cca36532939ba43ef9557def9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
