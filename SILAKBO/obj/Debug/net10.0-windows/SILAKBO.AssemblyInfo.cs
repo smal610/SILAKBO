27aa22c64bf216b31d4cc95687fc4500fc84d174
villanuevaahdlei6-sketch <villanuevaahdlei6@gmail.com>
2026-03-19 12:28:10 +0800

    Updated track case form(added functions and redesigned). Adjusted functions in user dashboard,login,report repository and report service

--- SILAKBO/obj/Debug/net10.0-windows/SILAKBO.AssemblyInfo.cs
+++ SILAKBO/obj/Debug/net10.0-windows/SILAKBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968c39af55a303dbcb8c82e038b6fcbebb3028ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c670f4032f1c184c61a4d893312a6a04782222")]
 [assembly: System.Reflection.AssemblyProductAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SILAKBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
